1eee466a2af586bcf3fbe76116795a619042ba8c
sungbu <609103450@qq.com>
2022-12-13 14:22:49 +0800

    no

--- src/testWIfi.java
+++ src/testWIfi.java
@@ -2,13 +2,11 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.Set;
 
 public class testWIfi {
     public static void main(String[] args) throws Exception {
         InetAddress host = InetAddress.getLocalHost();//获得本机的InetAddress对象
+        System.out.println(host);
         String hostAddress = host.getHostAddress();//获取的本机的IP地址
         int pos = hostAddress.lastIndexOf(".");//获取IP地址中最后一个点的位置  9 
         String wd = hostAddress.substring(0, pos + 1); // 对本机的IP进行截取，获取网段 172.20.10.
